604d937275563b4525b88c06e5f356d78adc75a4
mfasel <markus.fasel@cern.ch>
2015-12-09 14:40:23 +0100

    Add sample definition in order to steer the full train

---
+++ train/steer/sample.py
@@ -0,0 +1,57 @@
+'''
+Created on 09.12.2015
+
+@author: markusfasel
+'''
+
+import json
+
+class Sample(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, filename = None):
+        '''
+        Constructor
+        '''
+        self.__name = ""
+        self.__year = 0
+        self.__splitlevel = 10
+        self.__filelists = []
+        if filename:
+            self.__init(filename)
+        
+    def Init(self, filename):
+        jsonstring = ""
+        reader = open(filename)
+        for line in reader:
+            line = line.replace("\n", "")
+            line = line.rstrip().lstrip()
+            if not len(line):
+                continue
+            jsonstring += line
+        reader.close()
+        jsontree = json.loads(jsonstring)
+        for k, v in jsontree:
+            if k.upper() == "NAME":
+                self.__name = str(v)
+            elif k.upper() == "SPLITLEVEL":
+                self.__splitlevel = int(v)
+            elif k.upper() == "YEAR":
+                self.__year = int(v)
+            elif k.upper() == "FILELISTS":
+                for l in v:
+                    self.__filelists.append(l)
+                    
+    def GetName(self):
+        return self.__name
+    
+    def GetYear(self):
+        return self.__year
+    
+    def GetFilelists(self):
+        return self.__filelists
+    
+    def GetSplitLevel(self):
+        return self.__splitlevel
