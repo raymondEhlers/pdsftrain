9cb608b07d63c67bf48c2db7dae58ce0ef459441
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2015-12-10 16:32:27 +0100

    Incremental updates to my AddTask macro
    It now causes the analysis task to compile when it is placed within the
    directory. Still to be tested.

--- rehlers/AddTaskEmcalInvariantMass.C
+++ rehlers/AddTaskEmcalInvariantMass.C
@@ -1,5 +1,19 @@
 void AddTaskEmcalInvariantMass()
 {
+    const char * trainRoot = gSystem->Getenv("TRAIN_ROOT");
+    if (trainRoot != 0)
+    {
+        gSystem->AddIncludePath(Form("%s/rehlers", trainRoot));
+        Printf("Added rehlers user directory to includes.");
+        gSystem->Load(Form("%s/AliAnalysisTaskEmcalInvariantMass.cxx++", trainRoot));
+    }
+    else
+    {
+        Printf("Failed to include rehlers user directory");
+        return;
+    }
+
+    // Use ++ to only compile if necessary
     TString clustersName = "CaloClusters";
     gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEmcalInvariantMass.C");
     AliAnalysisTaskEmcalInvariantMass* anaTask = AddTaskEmcalInvariantMass(/*tracksName,*/clustersName, 1);
