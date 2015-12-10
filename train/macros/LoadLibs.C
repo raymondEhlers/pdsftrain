cb931dbff5e151abf57846faa6c8ded37f9295db
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2015-12-10 22:50:25 +0100

    Remove unnecessary files.

--- train/macros/LoadLibs.C
+++ train/macros/LoadLibs.C
@@ -4,12 +4,7 @@ void LoadLibs(){
 	 */
 
 	TString libs[] = {"CGAL","fastjet","siscone","siscone_spherical","fastjetplugins","fastjettools","fastjetcontribfragile"};
-    /*TString extension = ".so";
-    if (gSystem->GetFromPipe("uname -s") == "Darwin")
-    {
-        extension = ".dylib";
-    }*/
-	for(TString *libiter = libs; libiter < libs + sizeof(libs)/sizeof(TString); libiter++){
+    for(TString *libiter = libs; libiter < libs + sizeof(libs)/sizeof(TString); libiter++){
 		gSystem->Load(Form("lib%s", libiter->Data()));
 	}
 }
