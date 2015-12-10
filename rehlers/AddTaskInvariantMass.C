39d90dc1b544586050b1af8a197177712acb9aaa
mfasel <markus.fasel@cern.ch>
2015-12-11 00:05:19 +0100

    Outsource build of user task

--- rehlers/AddTaskInvariantMass.C
+++ rehlers/AddTaskInvariantMass.C
@@ -6,17 +6,20 @@ void AddTaskInvariantMass()
     Printf("Train root: %s", trainRoot);
     if (trainRoot != 0)
     {
+    	/*
         Printf("Added rehlers user directory to includes and compiling.");
         gSystem->AddIncludePath(Form("-I%s/rehlers", trainRoot));
         gSystem->AddIncludePath(Form("-I%s/include", gSystem->Getenv("ALICE_ROOT")));
         gSystem->AddIncludePath(Form("-I%s/include", gSystem->Getenv("ALICE_PHYSICS")));
         gROOT->LoadMacro(Form("%s/rehlers/AliAnalysisTaskEmcalInvariantMass.cxx++", trainRoot));
+        */
         
         // Use ++ to only compile if necessary
         TString clustersName = "CaloClusters";
         gROOT->LoadMacro(Form("%s/rehlers/AddTaskEmcalInvariantMass.C", trainRoot));
         AliAnalysisTaskEmcalInvariantMass* anaTask = AddTaskEmcalInvariantMass(/*tracksName,*/clustersName, 1);
     }
+
     else
     {
         Printf("Failed to include rehlers user directory");
